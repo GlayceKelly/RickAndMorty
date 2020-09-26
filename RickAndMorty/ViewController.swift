6bd1488a2e589b10b13e39e76fcc0df70cd53a4b
@tglaycekds <tglaycekds@fastshop.com.br>
2020-09-26 00:30:11 -0300

    Initial Commit

---
+++ RickAndMorty/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RickAndMorty
+//
+//  Created by Glayce Kelly on 26/09/20.
+//  Copyright © 2020 Glayce Kelly. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
